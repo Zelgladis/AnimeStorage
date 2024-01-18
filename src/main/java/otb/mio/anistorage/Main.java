ee85fe817ef16a63ebf4fae29998ad21bb0f8f1b
Zelgladis <Cep908@gmail.com>
2024-01-18 23:56:28 +0400

    initial commit

--- src/main/java/otb/mio/anistorage/Main.java
+++ src/main/java/otb/mio/anistorage/Main.java
@@ -3,14 +3,19 @@ package otb.mio.anistorage;
 import otb.mio.anistorage.form.Jinfo;
 import otb.mio.anistorage.db.Sqliter;
 
+import java.io.IOException;
+
+
 public class Main
 {
-    public static void main(String[] args)
-    {
-        Jinfo jinfo = new Jinfo();
-        jinfo.setVisible(true);
+    public static void main(String[] args) throws IOException {
+//        Jinfo jinfo = new Jinfo();
+//        jinfo.setVisible(true);
+        String root_path = System.getProperty("user.dir");
+        String bd_path = root_path + "/src/main/resources/bd/Test.db";
+        System.out.println(bd_path);
 
-        Sqliter sqliter = new Sqliter();
+        Sqliter sqliter = new Sqliter(bd_path);
 
     }
 }
--- src/main/java/otb/mio/anistorage/db/Sqliter.java
+++ src/main/java/otb/mio/anistorage/db/Sqliter.java
@@ -1,5 +1,7 @@
 package otb.mio.anistorage.db;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,12 +9,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 public class Sqliter
 {
-    public static Connection conn;
-    public static Statement statmt;
-    public static ResultSet resSet;
+    public Connection conn;
+    public Statement statmt;
+    public ResultSet resSet;
 
-    public Sqliter()
+    public String bd_path;
+
+    public Sqliter(String bd_path)
     {
+        this.bd_path = bd_path;
+        boolean res = this.bd_file();
+
         try {
             conn();
         } catch (ClassNotFoundException | SQLException ex)
@@ -20,12 +27,56 @@ public class Sqliter
             System.out.println(ex);
         }
     }
-    public static void conn() throws ClassNotFoundException, SQLException
-    {
-        conn = null;
-        Class.forName("org.sqlite.JDBC");
-        conn = DriverManager.getConnection("jdbc:sqlite:data/bd/AlohaAnime.db");
 
-        System.out.println("База Подключена!");
+    private boolean bd_file()
+    {
+        try {
+            File file = new File(bd_path);
+            if (!file.exists()) {
+                file.createNewFile();
+                conn();
+                new_dbase();
+            }
+            return true;
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
     }
+
+    private void new_dbase()
+    {
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+        try
+        {
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void conn() throws ClassNotFoundException, SQLException
+    {
+        if (conn == null || conn.isClosed()) {
+            //conn = null;
+            //Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:"+this.bd_path);
+            System.out.println("База Подключена!");
+        }
+    }
+
+
+
 }
